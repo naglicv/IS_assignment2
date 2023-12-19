80a53a50bb18c7badecb98ec2ee2f6048034c703
ArjanSkok <arjan.skok@gmail.com>
2023-12-19 19:41:41 +0100

    Vectorizer and random forest

--- main.py
+++ main.py
@@ -5,14 +5,15 @@ from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.linear_model import LogisticRegression
 from sklearn.metrics import accuracy_score, classification_report
 from datasets import load_dataset
-import json
-
 import nltk
 import string
 from nltk.corpus import stopwords
 from nltk.tokenize import word_tokenize
 from nltk.stem import WordNetLemmatizer
 from nltk.stem import PorterStemmer
+from sklearn.linear_model import LogisticRegression
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.metrics import accuracy_score
 
 nltk.download('punkt')
 nltk.download('stopwords')
@@ -46,12 +47,22 @@ def preprocess_text(text):
 
 df = pd.read_json("./datasets/News_Category_Dataset_IS_course.json", lines=True)
 
-new_data = df[['headline', 'short_description', 'category']].copy()
+new_data = df[['headline', 'short_description', 'category']][:50000].copy()
 
 # Replace None with a default string and apply function
-new_data['clean_headline'] = new_data['headline'].fillna('').apply(preprocess_text)
-new_data['clean_short_description'] = new_data['short_description'].fillna('').apply(preprocess_text)
-
-#new_data['clean_desc'] = new_data['short_description'].apply(preprocess_text)
+new_data['clean_head'] = new_data['headline'].fillna('').apply(preprocess_text)
+new_data['clean_desc'] = new_data['short_description'].fillna('').apply(preprocess_text)
  
-print(new_data[['clean_headline','headline', 'clean_short_description','short_description']][:10])
+print(new_data[['clean_head','headline', 'clean_desc','short_description']][:10])
+
+X_train, X_test, y_train, y_test = train_test_split(new_data[['clean_head', 'clean_desc', 'headline', 'short_description']], new_data['category'], test_size=0.2, random_state=42)
+
+vectorizer = TfidfVectorizer()
+X_train_vec = vectorizer.fit_transform(X_train['clean_desc'])
+X_test_vec = vectorizer.transform(X_test['clean_desc'])
+
+rf_model = RandomForestClassifier()
+rf_model.fit(X_train_vec, y_train)
+rf_predictions = rf_model.predict(X_test_vec)
+rf_accuracy = accuracy_score(y_test, rf_predictions)
+print("Random Forest Accuracy:", rf_accuracy)
