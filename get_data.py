2fb65aefc4a15841458810ecd5b1ed4743a4b6db
ArjanSkok <arjan.skok@gmail.com>
2023-12-29 18:44:14 +0100

    Updated getdata

--- get_data.py
+++ get_data.py
@@ -8,6 +8,8 @@ from nltk.stem import PorterStemmer
 from sklearn.model_selection import train_test_split
 from gensim.models import Word2Vec
 import models
+from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import accuracy_score, classification_report
 
 nltk.download('punkt')
 nltk.download('stopwords')
@@ -49,9 +51,21 @@ def get_data():
 
     return new_data
 
+def get_average_w2v(tokens, w2v_model):
+        vector_sum = 0
+        count = 0
+        for word in tokens:
+            if word in w2v_model.wv:
+                vector_sum += w2v_model.wv[word]
+                count += 1
+        if count != 0:
+            return vector_sum / count
+        else:
+            return [0] * 100  # Return zero vector if no word found
+
 
 def get_data_w2v():
-    new_data = get_data()[:1000]
+    new_data = get_data()
 
     X_train, X_test, y_train, y_test = train_test_split(new_data[['clean_head', 'clean_desc', 'headline', 'short_description']], new_data['category'], test_size=0.2, random_state=42)
 
@@ -64,20 +78,16 @@ def get_data_w2v():
     w2v_model_train = Word2Vec(tokenized_train_text, vector_size=100, window=5, min_count=1, workers=4, epochs=10)
     w2v_model_test = Word2Vec(tokenized_test_text, vector_size=100, window=5, min_count=1, workers=4, epochs=10)
 
-    all_words = w2v_model_train.wv.index_to_key
-    word_vectors = {word: w2v_model_train.wv[word] for word in all_words}
+    # Add Word2Vec representations to DataFrame
+    X_train_w2v = [get_average_w2v(text, w2v_model_train) for text in tokenized_train_text]
+    X_test_w2v = [get_average_w2v(text, w2v_model_test) for text in tokenized_test_text]
 
-    print(word_vectors)
-
-    # Print the word vectors
-    '''for word in word_vectors.items():
-        print(f"Word: {word}")
-        print(f"Vector: {vector}")
-        print("\n")  # Add a newline for better readability
-
-    similar_words = w2v_model_train.wv.most_similar('cat', topn=10)
-    for word, similarity in similar_words:
-        print(f"Similar word: {word}, Similarity: {similarity}")'''
+    # Logistic Regression model
+    logistic_model = LogisticRegression()
+    logistic_model.fit(X_train_w2v, y_train)
+    logistic_predictions = logistic_model.predict(X_test_w2v)
+    logistic_accuracy = accuracy_score(y_test, logistic_predictions)
+    print("Logistic Regression Accuracy:", logistic_accuracy)
 
 if __name__ == "__main__":
     get_data_w2v()
