cf4fba84b29f879589022bd8d98af39cc9b22958
ArjanSkok <arjan.skok@gmail.com>
2023-12-29 17:16:51 +0100

    XGboosting

--- main.py
+++ main.py
@@ -4,12 +4,14 @@ from sklearn.model_selection import train_test_split, cross_val_score, GridSearc
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.linear_model import LogisticRegression
 from sklearn.metrics import accuracy_score, classification_report
+from sklearn.preprocessing import LabelEncoder
+from xgboost import XGBClassifier
 
 from sklearn.linear_model import LogisticRegression
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.metrics import accuracy_score
 
-df = pd.read_csv("./datasets/cleaned.csv")
+df = pd.read_csv("./datasets/cleaned.csv")[:50000]
 
 print(df[['clean_head','headline', 'clean_desc','short_description']][:10])
 
@@ -27,13 +29,15 @@ X_train_vec = vectorizer.fit_transform(X_train['clean_text'].values.astype('U'))
 X_test_vec = vectorizer.transform(X_test['clean_text'].values.astype('U'))
 """
 # Random Forest Classifier
+print("starting random forest")
 rf_model = RandomForestClassifier()
 rf_model.fit(X_train_vec, y_train)
 rf_predictions = rf_model.predict(X_test_vec)
 rf_accuracy = accuracy_score(y_test, rf_predictions)
-print("Random Forest Accuracy:", rf_accuracy)
-"""
+print("Random Forest Accuracy:", rf_accuracy)"""
+
 # Logistic Regression
+print("starting Logistic Regression")
 lr_model = LogisticRegression(max_iter=1000)
 lr_model.fit(X_train_vec, y_train)
 lr_predictions = lr_model.predict(X_test_vec)
@@ -44,3 +48,18 @@ print(classification_report(y_test, lr_predictions, zero_division=1))
 
 scores = cross_val_score(lr_model, X_train_vec, y_train, cv=5)
 print("Cross-Validation Scores:", scores)
+
+
+# XGBoosting
+label_encoder = LabelEncoder()
+y_encoded = label_encoder.fit_transform(df['category'])
+X = X_train.append(X_test, ignore_index=True)
+X = vectorizer.fit_transform(X['clean_text'].values.astype('U'))
+X_t, X_ts, y_t, y_ts = train_test_split(X, y_encoded, test_size=0.2, random_state=8678686)
+
+# XGBoost
+model_xgb = XGBClassifier()
+model_xgb.fit(X_t, y_t)
+pred_xgb = model_xgb.predict(X_ts)
+result_xgb = accuracy_score(y_ts, pred_xgb)
+print("XGBoost Accuracy:", result_xgb)
