c3ffb7373ec1e5d7ebd62c869dd47adadad0960c
ArjanSkok <arjan.skok@gmail.com>
2024-01-13 21:05:12 +0100

    Added files and pictures

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ from sklearn.linear_model import LogisticRegression
 from sklearn.metrics import accuracy_score, classification_report
 from sklearn.preprocessing import LabelEncoder
 from xgboost import XGBClassifier
+import models
 
 from sklearn.linear_model import LogisticRegression
 from sklearn.ensemble import RandomForestClassifier
@@ -13,7 +14,7 @@ from sklearn.metrics import accuracy_score
 
 df = pd.read_csv("./datasets/cleaned.csv")[:50000]
 
-print(df[['clean_head','headline', 'clean_desc','short_description']][:10])
+#print(df[['clean_head','headline', 'clean_desc','short_description']][:10])
 
 # Split the data into training and testing sets
 X_train, X_test, y_train, y_test = train_test_split(df[['clean_head', 'clean_desc', 'headline', 'short_description']], df['category'], test_size=0.2, random_state=42)
@@ -23,10 +24,13 @@ vectorizer = TfidfVectorizer()
 # Repeat the headline 2 times
 X_train['clean_text'] = X_train['clean_head'] + ' ' + X_train['clean_head'] + ' ' + X_train['clean_desc']
 X_test['clean_text'] = X_test['clean_head'] + ' ' + X_test['clean_head'] + ' ' + X_test['clean_desc']
-print(X_test['clean_text'])
 
 X_train_vec = vectorizer.fit_transform(X_train['clean_text'].values.astype('U'))
 X_test_vec = vectorizer.transform(X_test['clean_text'].values.astype('U'))
+
+print(X_train_vec)
+
+
 """
 # Random Forest Classifier
 print("starting random forest")
@@ -49,12 +53,11 @@ print(classification_report(y_test, lr_predictions, zero_division=1))
 scores = cross_val_score(lr_model, X_train_vec, y_train, cv=5)
 print("Cross-Validation Scores:", scores)
 
-
+'''
 # XGBoosting
 label_encoder = LabelEncoder()
 y_encoded = label_encoder.fit_transform(df['category'])
-X = X_train.append(X_test, ignore_index=True)
-X = vectorizer.fit_transform(X['clean_text'].values.astype('U'))
+X = X_train_vec.append(X_test_vec, ignore_index=True)
 X_t, X_ts, y_t, y_ts = train_test_split(X, y_encoded, test_size=0.2, random_state=8678686)
 
 # XGBoost
@@ -62,4 +65,4 @@ model_xgb = XGBClassifier()
 model_xgb.fit(X_t, y_t)
 pred_xgb = model_xgb.predict(X_ts)
 result_xgb = accuracy_score(y_ts, pred_xgb)
-print("XGBoost Accuracy:", result_xgb)
+print("XGBoost Accuracy:", result_xgb)'''
--- test2.py
+++ test2.py
@@ -1,22 +1,54 @@
+import numpy as np
 import pandas as pd
-from keras.preprocessing.text import one_hot
+from sklearn.model_selection import train_test_split, cross_val_score, GridSearchCV 
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import accuracy_score, classification_report
 
+from sklearn.linear_model import LogisticRegression
+from sklearn.ensemble import AdaBoostClassifier, BaggingClassifier, RandomForestClassifier, VotingClassifier
+from sklearn.metrics import accuracy_score
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.preprocessing import LabelEncoder
+from sklearn.naive_bayes import GaussianNB, MultinomialNB
+from sklearn.tree import DecisionTreeClassifier
+from xgboost import XGBClassifier
+import seaborn as sns
+import matplotlib.pyplot as plt
 
 df = pd.read_csv("./datasets/cleaned.csv")
-df = df[:15000].copy()
-text = df['clean_text'].str.cat(sep=', ')
+df = df[:10000].copy()
+#len = df.clean_text.map(len).max()
+#print(len)
 
-vocab_size = 336
-result = one_hot(text, round(vocab_size*1.3))
+X = df.drop('category', axis=1)
+#X['clean_text'] = X['clean_text'] + " " + X['clean_desc']
+y = pd.Categorical(df['category'])
 
-count = 0
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 
-l1 = []
- 
-# traversing the array
-for item in result:
-    if item not in l1:
-        count += 1
-        l1.append(item)
+vectorizer = TfidfVectorizer()
+X_train_vec = vectorizer.fit_transform(X_train['clean_text'].values.astype('U'))
+X_test_vec = vectorizer.transform(X_test['clean_text'].values.astype('U'))
 
-print(count)
+# Decision Tree
+
+
+'''dt = DecisionTreeClassifier(random_state=42)
+params = {
+    'max_depth': [10, 50, 100, 150],
+    'min_samples_split': [20, 30, 50, 100],
+    'criterion': ['gini', 'entropy']
+}
+grid_search = GridSearchCV(estimator=dt, 
+                           param_grid=params, 
+                           cv=4, n_jobs=-1, verbose=1, scoring = "accuracy")
+model_DT = DecisionTreeClassifier()
+grid_search.fit(X_train_vec, y_train)
+print("Decision Tree Accuracy:", grid_search.best_estimator_)'''
+
+model_DT = DecisionTreeClassifier(max_depth=10, min_samples_split=50, random_state=42)
+model_DT.fit(X_train_vec, y_train)
+pred_DT = model_DT.predict(X_test_vec)
+result_ca_DT = accuracy_score(y_test, pred_DT)
+print("Decision Tree Accuracy:", result_ca_DT)
